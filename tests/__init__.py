f7ce2e19d69895dba2f81ba7a6a869c0f4ca2736
ItzRustam <rustambhadouriya7@gmail.com>
2026-08-26 01:04:32 -0400

    Test Suite with Docker Automation

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
 from .auth_tests import auth_test
+from .logger import get_logger
 
-__all__ = ["auth_test"]
+# Added logger for colorfull logging in test cases
+__all__ = ["auth_test", 'get_logger']
---
+++ tests/logger.py
@@ -0,0 +1,36 @@
+import logging
+import colorlog
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.DEBUG)
+
+    handler = colorlog.StreamHandler()
+
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s"
+        "%(asctime)s "
+        "[%(levelname)-8s] "
+        "%(name)s: "
+        "%(message)s",
+        datefmt="%H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.propagate = False
+
+    return logger
